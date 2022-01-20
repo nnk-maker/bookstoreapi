3a46f9f57c3fc842ee7115cb9cd4fbe74f52ae97
nnk-maker <krishnanarasimha4@gmail.com>
2022-01-20 11:16:25 +0100

    Author resources commit
    Aurthor resources

--- bookstore/routes/api.php
+++ bookstore/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthorsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/* Route::get('/test',function(Request $request){
+    return 'Authenticated';
+}); */
+
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+
+    Route::get('/authors/{author}', [AuthorsController::class, 'show']);
+    //Route::apiResource('/authors', AuthorsController::class);
+    //Route::apiResource('/books', BooksController::class);
 });
+
+
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
